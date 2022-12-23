7eddd3cf781e35158cc9940dbdb603905789e103
adamjgentz <adamjgentz@gmail.com>
2022-12-23 12:09:39 -0600

    Fixed diagonal move speed issue (it doubled cause of vector maths)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,34 +7,26 @@ public class CharacterController : MonoBehaviour
 
     [SerializeField]
     float moveSpeed = 4f;
-
     Vector3 forward, right;
 
-    void Start()
+    void Update ()
     {
+       
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0,90,0)) * forward;
-    }
-
-    void Update()
-    {
-        if(Input.anyKey)
-            Move();
-    }
-
-    void Move()
-    {
-        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
-
-        Vector3 heading = Vector3.Normalize(Vector3.Scale(rightMovement, upMovement));
-
-        if(heading != Vector3.zero)
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (direction.magnitude > 0.1f)
+        {
+            Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+            Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+          
+            Vector3 heading = Vector3.Normalize(rightMovement+upMovement);
             transform.forward = heading;
-        transform.position += rightMovement;
-        transform.position += upMovement;
+            transform.position += heading * moveSpeed * Time.deltaTime; 
+        }
+       
     }
 }
